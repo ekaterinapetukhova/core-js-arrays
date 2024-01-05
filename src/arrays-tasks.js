d932363a6b54e4c53f6c82aaf51d5a2e72178607
Ekaterina <katepthv12@gmail.com>
2024-01-05 19:34:31 +0100

    feat: solve getAverage task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -132,8 +132,14 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length < 1) {
+    return 0;
+  }
+
+  const sum = arr.reduce((acc, item) => acc + item);
+
+  return Number((sum / arr.length).toFixed(2));
 }
 
 /**
