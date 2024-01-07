74611600f383da919cbefbba572e9a8a796d18fe
Ekaterina <katepthv12@gmail.com>
2024-01-07 13:48:24 +0100

    feat: solve generateOdds task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -382,8 +382,10 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len * 2 }, (_, index) => index).filter(
+    (item) => item % 2 !== 0
+  );
 }
 
 /**
